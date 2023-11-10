91d3c2c588e442983b4beeaa2f80ac0d6837e281
Sweep <joshuapatrickchiu76@gmail.com>
2023-11-11 00:12:43 +0800

    added changes

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+
+
+
+
+function App() {
+  
+  return (
+   <div>
+    <Dashboard/>
+    <Login/>
+    <Register/>
+   </div>
+  )
+}
+
+export default App
